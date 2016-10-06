7ba6c120cb7e5bc8c797c59a023064b2b646cd1e
Andy Eccleston <eandy5000@yahoo.com>
2016-10-06 09:48:27 -0500

    server and homeroute function

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ http.createServer(function(req, res){
     res.end("Hi...\n");
 }).listen(3000);
 console.log("working");
+
+function home(req, res){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Header\n");
+    res.write("Search\n");
+    res.end("Footer\n");
+};
