9ca6dcc5024941f6b7bf8a4871f40beebab85e89
Andy Eccleston <eandy5000@yahoo.com>
2016-10-06 10:02:27 -0500

    routing before seperation of concerns

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 var http = require('http');
 
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-   // res.write("another message");
-    res.end("Hi...\n");
+    home(req, res);
+    about(req,res);
+    user(req, res);
 }).listen(3000);
 console.log("working");
 
 function home(req, res){
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Header\n");
-    res.write("Search\n");
-    res.end("Footer\n");
+
+    if(req.url === "/"){ 
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write("Header\n");
+        res.write("Search\n");
+        res.end("Footer\n");
+    }
 };
+
+function about(req, res){
+    if(req.url === "/about"){
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write("About\n");
+        res.end("content\n");
+    }
+}
+
+function user(req, res){
+    var username = req.url.replace("/", "");
+    if (username.length > 0 ){
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write(username + "\n");
+        res.end("content\n");
+    }
+}
